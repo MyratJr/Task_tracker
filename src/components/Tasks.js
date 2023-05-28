7fe6e3c92957112dac5b66b7224aa8fdea9f6752
MyratJr <begmyradowmyratgeldi@gmail.com>
2023-05-28 19:41:37 +0500

    Task_Tracker

---
+++ src/components/Tasks.js
@@ -0,0 +1,16 @@
+import Task from './Task'
+
+const Tasks = ({tasks,onDelete,onToggle}) => {
+  return (
+    <>
+        {tasks.map((task,index)=>(
+            <Task key={index}
+                onDelete={onDelete} 
+                onToggle={onToggle} 
+                task={task}/>
+        ))} 
+    </>
+  )
+}
+
+export default Tasks
